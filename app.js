2db97f7e69c3ded000c1d05847f780e9e62d8c2f
Student <student@itpark.ru>
2019-09-28 10:57:24 +0300

    Переименование переменных

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 console.log('worked');
 
-const author = {
+const buzova = {
     id: 'buzova86',
     avatarUrl: https://scontent-arn2-1.cdninstagram.com/vp/e24c6f401983834e27247c3542022660/5E1B7CB0/t51.2885-19/s150x150/69672885_368328154091538_8485268536803983360_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com,
     verified: true,
@@ -9,7 +9,7 @@ const author = {
 
 const photo = {
     id: 'B23d82NooNF',
-    author: author,
+    author: buzova,
     location: 'Набережная Амура. Граница с Китаем',
     imageUrl: 'https://placekitten.com/800/800',
     videoUrl: null,
@@ -18,7 +18,7 @@ const photo = {
 
 const video = {
     id: 'B23d82NooNX',
-    author: author,
+    author: buzova,
     location: 'Торгово-развлекательный Центр Острова',
     imageUrl: 'https://placekitten.com/800/800',
     videoUrl: 'https://youtube.com/...',
